5738ff94af7e33fefe1c471833a49a8df1937f2f
Rushi20001 <rajwaderishi17@gmail.com>
2023-01-02 17:53:15 +0530

    import server url

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ var btnTranslate= document.querySelector("#btn-translate")
 var txtInput= document.querySelector("#txt-input")
 var txtoutput= document.querySelector("#output")
 
-
+var serverURL="https://api.funtranslations.com/translate/valyrian.json"
