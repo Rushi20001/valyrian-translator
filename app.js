afe6c6b3c3d86dad475bd50930be52804c0ee9a7
Rushi20001 <rajwaderishi17@gmail.com>
2023-01-02 18:08:41 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 var btnTranslate= document.querySelector("#btn-translate")
 var txtInput= document.querySelector("#txt-input")
-var txtoutput= document.querySelector("#output")
+var txtOutput= document.querySelector("#output")
 
 var serverURL="https://api.funtranslations.com/translate/valyrian.json"
+
+function getTranslationURL(text){
+    return serverURL + "?"+ "text=" + text
+}
+
+function clickHandler(){
+    var inputText = txtInput.value
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated
+        txtOutput.innerText = translatedText
+
+    })
+}
+ btnTranslate.addEventListener("click",clickHandler)
